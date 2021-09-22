a1fc8481310c9faf7034f834da907ed4effaa4a4
peter <pjhope@loyola.edu>
2021-09-22 12:20:02 -0400

    fixed print statements

--- PA1.py
+++ PA1.py
@@ -7,7 +7,6 @@
 print ("This Program will calculate the area that needs to be painted in a room and how much primer and paint will be needed to do it!")
 
 # import math module
-
 import math
 
 # ask for inputs of length, width, and height
@@ -15,16 +14,21 @@ print ("Please input the dimensions of the room in feet!")
 lengthStr = input("Length: ")
 widthStr = input("Width: ")
 heightStr = input("Height: ")
+
 #convert strings to floats
 length = float (lengthStr)
 width = float (widthStr)
 height = float (heightStr)
+
 # calculate the total area to be painted
 totalArea = (length*width)+(length*height*2)+(width*height*2)
+
 # calculate gallons of primer needed
 primerGal = totalArea/200
+
 # calculate gallons of paint needed
 paintGal = totalArea/350
+
 # print all outputs
 print ("The total area to be painted is:", totalArea, "square feet")
 print ("The number of gallons of primer needed is: %.2f" % primerGal, "gallons")
